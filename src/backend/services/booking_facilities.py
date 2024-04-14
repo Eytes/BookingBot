fdbfd04a06d56e76e804b6ef334c668d98b7c6ef
Eytes <yura.shamanov2000@bk.ru>
2024-04-15 00:04:28 +0300

    удаление интерфейсов

--- src/backend/services/booking_facilities.py
+++ src/backend/services/booking_facilities.py
@@ -1,10 +1,3 @@
-from .interfaces import GetByIdInterface
-from ..base_types import ItemId, Schema
-
-
-class BookingFacilitiesService(GetByIdInterface):
+class BookingFacilitiesService:
     def __init__(self, registry) -> None:
         self.__registry = registry
-
-    async def get_by_id(self, object_id: ItemId) -> Schema:
-        pass
--- src/backend/services/user.py
+++ src/backend/services/user.py
@@ -1,10 +1,3 @@
-from .interfaces import GetByIdInterface
-from ..base_types import ItemId, Schema
-
-
-class UserService(GetByIdInterface):
+class UserService:
     def __init__(self, registry) -> None:
         self.__registry = registry
-
-    async def get_by_id(self, object_id: ItemId) -> Schema:
-        pass
