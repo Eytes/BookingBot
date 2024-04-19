f793c91075f870452bc5fd1cd0dcfea3b96b93db
Eytes <yura.shamanov2000@bk.ru>
2024-04-20 01:58:55 +0300

    fix flake8

--- src/backend/registry/__init__.py
+++ src/backend/registry/__init__.py
@@ -1,4 +1,7 @@
-from .async_mongo_registry import AsyncMongoRegistryFactory, AsyncMongoRegistry
+from .async_mongo_registry import (
+    AsyncMongoRegistryFactory,  # noqa: F401
+    AsyncMongoRegistry,  # noqa: F401
+)
 from .mongo_helper import mongo_helper  # noqa: F401
 
 mongo_registry_factory = AsyncMongoRegistryFactory()
