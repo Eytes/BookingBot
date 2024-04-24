e26a78cc1215762e77ec1afb60341f189ff012a5
Eytes <yura.shamanov2000@bk.ru>
2024-04-24 16:19:03 +0300

    добавление аннотаций

--- src/backend/registry/mongo_helper.py
+++ src/backend/registry/mongo_helper.py
@@ -1,21 +1,25 @@
 from typing import AsyncGenerator
 
-from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorClientSession
+from motor.motor_asyncio import (
+    AsyncIOMotorClient,
+    AsyncIOMotorClientSession,
+    AsyncIOMotorDatabase,
+)
 
 from ..config import settings
 
 
-class MongoHelper:
+class AsyncMongoHelper:
     def __init__(self, client_url: str, database_name: str) -> None:
         self.__mongo_client = AsyncIOMotorClient(client_url)
         self.__mongo_database = self.__mongo_client[database_name]
 
     @property
-    def client(self):
+    def client(self) -> AsyncIOMotorClient:
         return self.__mongo_client
 
     @property
-    def database(self):
+    def database(self) -> AsyncIOMotorDatabase:
         return self.__mongo_database
 
     async def get_session(self) -> AsyncGenerator[AsyncIOMotorClientSession, None]:
@@ -25,7 +29,7 @@ class MongoHelper:
             await session.end_session()
 
 
-mongo_helper = MongoHelper(
+mongo_helper = AsyncMongoHelper(
     client_url=settings.mongodb.url,
     database_name=settings.mongodb.database_name,
 )
