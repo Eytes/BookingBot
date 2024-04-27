622ce53a6e45fb2b1b2227c5bb9371a3b7b7a1b2
Eytes <yura.shamanov2000@bk.ru>
2024-04-27 18:11:21 +0300

    создание сигнатур

--- src/backend/services/booking_facilities.py
+++ src/backend/services/booking_facilities.py
@@ -1,7 +1,24 @@
+from ..base_types import ItemId
 from ..registry import AsyncRegistry, AsyncDBHelper
+from ..schemas.booking_facilities import (
+    BookingFacilitiesSchema,
+    CreateBookingFacilitiesSchema,
+)
 
 
 class BookingFacilitiesService:
     def __init__(self, registry: AsyncRegistry, db_helper: AsyncDBHelper) -> None:
         self.__registry = registry
         self.__db_helper = db_helper
+
+    async def get_by_id(
+        self, booking_facilities_id: ItemId
+    ) -> BookingFacilitiesSchema: ...
+
+    async def create(
+        self, new_booking_facilities: CreateBookingFacilitiesSchema
+    ) -> CreateBookingFacilitiesSchema: ...
+
+    async def delete_by_id(
+        self, booking_facilities_id: ItemId
+    ) -> BookingFacilitiesSchema: ...
--- src/backend/services/user.py
+++ src/backend/services/user.py
@@ -1,7 +1,15 @@
+from ..base_types import ItemId
 from ..registry import AsyncRegistry, AsyncDBHelper
+from ..schemas.user import UserSchema, CreateUserSchema
 
 
 class UserService:
     def __init__(self, registry: AsyncRegistry, db_helper: AsyncDBHelper) -> None:
         self.__registry = registry
         self.__db_helper = db_helper
+
+    async def get_by_id(self, user_id: ItemId) -> UserSchema: ...
+
+    async def create(self, new_user: CreateUserSchema) -> UserSchema: ...
+
+    async def delete_by_id(self, user_id: ItemId) -> UserSchema: ...
