df60383dc7e0f1213f826975c4d6a634ce2d88b3
Furlos <levprimak1007@gmail.com>
2024-06-17 15:55:21 +0200

    Добавил новый тип PhoneType для будущего конекта с пользователями

--- src/backend/base_types.py
+++ src/backend/base_types.py
@@ -1,4 +1,7 @@
 from typing import TypeAlias, Annotated
 from uuid import UUID
 
+from pydantic_extra_types import phone_numbers
+
 ItemId: TypeAlias = Annotated[str, UUID.hex]
+PhoneType: phone_numbers
