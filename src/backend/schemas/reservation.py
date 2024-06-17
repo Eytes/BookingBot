2e29ccd7857d5e7f39e5d001e1d4ae615a7e8084
Furlos <levprimak1007@gmail.com>
2024-06-17 15:56:48 +0200

    В схему добавил ID пользователей и аудиторий, для обьеденения

--- src/backend/schemas/reservation.py
+++ src/backend/schemas/reservation.py
@@ -3,11 +3,14 @@ from datetime import datetime
 from pydantic import BaseModel
 
 from .mixins import MixinId
+from ..base_types import ItemId
 
 
 class ReservationBaseSchema(BaseModel):
     since_datetime: datetime
     until_datetime: datetime
+    audience_id: ItemId
+    user_id: ItemId | None
 
 
 class ReservationSchema(ReservationBaseSchema, MixinId):
