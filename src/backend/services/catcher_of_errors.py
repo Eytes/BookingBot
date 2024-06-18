04a29c748ccb5e592f6cf222459ff0aeaf395513
Furlos <levprimak1007@gmail.com>
2024-06-18 12:14:55 +0200

    Создал файл который ва будущем будет шаблоном для отвлавливания ошибок

---
+++ src/backend/services/catcher_of_errors.py
@@ -0,0 +1,9 @@
+# class Catcher_Of_Errors:
+#     def pattern_of_catcher(self,url, httpcode):
+#         try:
+#             urlopen(f"{url}")
+#         except HTTPError as err:
+#             if err.code == httpcode:
+#                 return f"Ошибка номер {httpcode}, попробуйте заново"
+#             else:
+#                 return False
