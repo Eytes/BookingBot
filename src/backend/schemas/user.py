6cbc15b68b9577420a8e870a5e6d0cbb31b05416
Furlos <levprimak1007@gmail.com>
2024-06-21 10:30:54 +0200

    Добавил номер телефона в схемы пользователей

--- src/backend/schemas/user.py
+++ src/backend/schemas/user.py
@@ -1,16 +1,18 @@
 from pydantic import BaseModel
 
+from backend.base_types import PhoneType
 from backend.schemas.mixins import MixinId
 
 
 class UserBaseSchema(BaseModel):
     surname: str
     name: str
+    phone_number: PhoneType
 
 
 class UserSchema(UserBaseSchema, MixinId):
     pass
 
 
-class UserCreateSchema(UserSchema):
+class CreateUserSchema(UserSchema):
     pass
