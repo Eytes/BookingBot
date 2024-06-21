e97dae3b17db157168adadd62fc3cf4aa003e97b
Furlos <levprimak1007@gmail.com>
2024-06-21 10:30:00 +0200

    Добавил колекцию пользователя

--- src/backend/repositories/__init__.py
+++ src/backend/repositories/__init__.py
@@ -6,3 +6,4 @@ client = pymongo.MongoClient(settings.mongodb.url)
 booking_bot_db = client[settings.mongodb.database_name]
 reservation_collection = booking_bot_db["reservations"]
 audience_collection = booking_bot_db["audiences"]
+user_collection = booking_bot_db["users"]
